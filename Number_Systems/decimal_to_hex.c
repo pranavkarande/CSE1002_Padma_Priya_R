6ce372e760feeeab1c9a4a213fd6d92b1ace4790
pranavkarande <pranav.3943@gmail.com>
2021-03-23 17:22:41 +0530

    Added decimal_to_hex

--- Number_Systems/decimal_to_hex.c
+++ Number_Systems/decimal_to_hex.c
@@ -4,22 +4,27 @@
 int main(void) {
   // Taking int as input
   int inp;
-  printf("Enter decimal number to be converted to binary: ");
+  printf("Enter decimal number to be converted to hexadecimal: ");
   scanf("%d", &inp);
 
   // Creating array for output and in for len of that array
   char out[100];
   int len = 0;
-
-  // Adding binary digits to out
-  while(inp>0) {
-    out[len] = (char)(inp % 2 + 48);
-    inp/=2;
+  int temp;
+  // Adding hex digits to out
+  while (inp > 0) {
+    temp = inp % 16;
+    if(temp < 10) {
+      out[len] = (char)(temp + 48);
+    } else {
+      out[len] = (char)(temp + 55);
+    }
+    inp /= 16;
     len++;
   }
 
   // Printing out in reverse order
-  printf("Decimal converted to binary is: ");
+  printf("Decimal converted to hexadecimal is: ");
   int i;
   for (i = strlen(out) - 1; i > -1; --i) {
     printf("%c", out[i]);

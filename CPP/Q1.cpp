dcccae3847816c5979b292fc2b0e2ec5afdf1dca
pranavkarande <pranav.3943@gmail.com>
2021-04-17 15:18:50 +0530

    Added CPP

---
+++ CPP/Q1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+
+class Employee {
+  int ID;
+  char Name[50];
+  int Age;
+  float Sal;
+  char Design[50];
+  char Gender;
+  int YoJ;
+
+  public:
+
+  void store() {
+    cout << "Enter ID: ";
+    cin >> ID;
+    cout << "Enter Name: ";
+    cin >> Name;
+    cout << "Enter Age: ";
+    cin >> Age;
+    cout << "Enter Designation: ";
+    cin >> Design;
+    cout << "Enter Gender: ";
+    cin >> Gender;
+    cout << "Enter Year of Joining: ";
+    cin >> YoJ;
+  }
+
+  void display () {
+    cout << ID << endl;
+    cout << Name << endl;
+    cout << Age << endl;
+    cout << Sal << endl;
+    cout << Design << endl;
+    cout << Gender << endl;
+    cout << YoJ << endl;
+  }
+
+  void calSalary() {
+    float basic;
+    cout << "Enter the basic value: ";
+    cin >> basic;
+    Sal = ((basic + 0.2*basic + 0.2*basic) - (0.2*basic));
+  }
+  void updateSalary() {
+    if (Gender == 'M' && Age > 45 && YoJ < 2004 && !strcmp(Design, "Manager")) {
+      Sal += 0.05*Sal;
+    }
+  }
+
+};
+
+int main(void) {
+  Employee e;
+
+  e.store();
+  cout << "------Before-------" << endl;
+  e.display();
+  e.calSalary();
+  cout << "------After-------" << endl;
+  e.display();
+}

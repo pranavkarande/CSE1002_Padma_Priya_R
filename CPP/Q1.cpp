cbff4bc4295f11a0081e2846479aebf51db8c585
pranavkarande <pranav.3943@gmail.com>
2021-04-20 17:14:05 +0530

    Formatted Q1

--- CPP/Q1.cpp
+++ CPP/Q1.cpp
@@ -12,47 +12,52 @@ class Employee {
   int YoJ;
 
   public:
-
-  void store() {
-    cout << "Enter ID: ";
-    cin >> ID;
-    cout << "Enter Name: ";
-    cin >> Name;
-    cout << "Enter Age: ";
-    cin >> Age;
-    cout << "Enter Designation: ";
-    cin >> Design;
-    cout << "Enter Gender: ";
-    cin >> Gender;
-    cout << "Enter Year of Joining: ";
-    cin >> YoJ;
-  }
-
-  void display () {
-    cout << ID << endl;
-    cout << Name << endl;
-    cout << Age << endl;
-    cout << Sal << endl;
-    cout << Design << endl;
-    cout << Gender << endl;
-    cout << YoJ << endl;
-  }
-
-  void calSalary() {
-    float basic;
-    cout << "Enter the basic value: ";
-    cin >> basic;
-    Sal = ((basic + 0.2*basic + 0.2*basic) - (0.2*basic));
-  }
-  void updateSalary() {
-    if (Gender == 'M' &&
-        Age > 45 && YoJ < 2004 &&
-        !strcmp(Design, "Manager")) {
-      Sal += 0.05*Sal;
-    }
-  }
-
+    void store();
+    void display ();
+    void calSalary();
+    void updateSalary();
 };
+
+void Employee::store() {
+  cout << "Enter ID: ";
+  cin >> ID;
+  cout << "Enter Name: ";
+  cin >> Name;
+  cout << "Enter Age: ";
+  cin >> Age;
+  cout << "Enter Designation: ";
+  cin >> Design;
+  cout << "Enter Gender: ";
+  cin >> Gender;
+  cout << "Enter Year of Joining: ";
+  cin >> YoJ;
+}
+
+void Employee::display() {
+  cout << ID << endl;
+  cout << Name << endl;
+  cout << Age << endl;
+  cout << Sal << endl;
+  cout << Design << endl;
+  cout << Gender << endl;
+  cout << YoJ << endl;
+}
+
+void Employee::calSalary() {
+  float basic;
+  cout << "Enter the basic value: ";
+  cin >> basic;
+  Sal = ((basic + 0.2 * basic + 0.2 * basic) - (0.2 * basic));
+}
+
+void Employee::updateSalary() {
+  if (Gender == 'M' &&
+      Age > 45 &&
+      YoJ < 2004 &&
+      !strcmp(Design, "Manager")) {
+    Sal += 0.05 * Sal;
+  }
+}
 
 int main(void) {
   Employee e;
